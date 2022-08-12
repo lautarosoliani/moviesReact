f7156bc41fb5b1314a63f9256ec8e1385791e342
lautarosoliani <lautarosoliani@hotmail.com>
2022-08-12 11:58:49 -0300

    refactor for deploy

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+// import App from './App'
+
+// const rootElement = document.getElementById('root')
+// const root = createRoot(rootElement)
+
+// root.render(
+//   <BrowserRouter>
+//     <StrictMode>
+//       <App />
+//     </StrictMode>
+//   </BrowserRouter>
+// )
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 
-const rootElement = document.getElementById('root')
-const root = createRoot(rootElement)
-
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
-    <StrictMode>
+    <React.StrictMode>
       <App />
-    </StrictMode>
+    </React.StrictMode>
   </BrowserRouter>
 )
